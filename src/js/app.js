e93661b599af9e3985f12b703528d4dab0f27094
Gaspar Ortmann <gasparortmann@gmail.com>
2023-07-25 12:37:01 -0300

    dark/light mode preferences

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,23 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark');
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    } else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        } else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-botton');
 
     botonDarkMode.addEventListener('click', function(){
