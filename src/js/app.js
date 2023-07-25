11f74a0c272dff8da38c9cd7a97ba0fa17ea6512
Gaspar Ortmann <gasparortmann@gmail.com>
2023-07-25 11:31:11 -0300

    burger menu toggle

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    eventListeners();
+});
+
+function eventListeners(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+
+    navegacion.classList.toggle('mostrar');
+}
