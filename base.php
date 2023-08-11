8f51cc4db3658f6e2188b3710acee47f23c4bebe
Gaspar Ortmann <gasparortmann@gmail.com>
2023-08-11 10:21:50 -0300

    restore project

---
+++ base.php
@@ -0,0 +1,12 @@
+<?php
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>TItulo Pagina</h1>
+</main>
+
+<?php
+    incluirTemplate('footer');
+?>
