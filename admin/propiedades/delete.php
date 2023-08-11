e6248d4a68d322615db14e70f791c512d2c163df
Gaspar Ortmann <gasparortmann@gmail.com>
2023-08-10 21:16:19 -0300

    backup

---
+++ admin/propiedades/delete.php
@@ -0,0 +1,12 @@
+<?php
+    require '../../includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Borrar</h1>
+</main>
+
+<?php
+    incluirTemplate('footer');
+?>
---
+++ anuncio.php
@@ -0,0 +1,40 @@
+<?php
+
+    !require 'includes/funciones.php';
+ 
+    incluirTemplate('header');
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Casa en venta frente al bosque</h1>
+
+        <picture>
+            <source srcset="build/img/destacada.webp" type="image/webp">
+            <source srcset="build/img/destacada.jpg" type="image/jpg">
+            <img loading="lazy" src="build/img/destacada.jpg" alt="imagen de la propiedad">
+        </picture>
+
+        <div class="resumen-propiedad">
+            <p class="precio">$3,000,000</p>
+            <ul class="iconos-caracteristicas">
+                <li>
+                    <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                    <p>3</p>
+                </li>
+                <li>
+                    <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                    <p>3</p>
+                </li>
+                <li>
+                    <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
+                    <p>4</p>
+                </li>
+            </ul>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni ullam sit fugiat excepturi dignissimos aliquid aspernatur hic eum omnis numquam sapiente corrupti eveniet dicta voluptates obcaecati architecto amet, repellat eaque.</p>
+        </div>
+    </main>
+
+    <?php
+    incluirTemplate('footer');
+    ?>
